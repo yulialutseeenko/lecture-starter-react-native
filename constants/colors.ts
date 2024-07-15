82063942b310ad9a861ff16e2cf82d097876eb1a
Dmitrij Revenets <revenets@gmail.com>
2024-07-15 12:39:36 +0300

    Changed colors const

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,27 +1,19 @@
 const COLORS_LIGHT = {
-	appBackground: "#EEECEC",
-	primary900: "#00146D",
-	primary300: "#6d78bd",
-	primary200: "#969ecf",
-	primary100: "#B0B7E5",
-	secondary800: "#6d0014",
-	grey600: "#757575",
-	grey300: "#BDBDBD",
-	fontMain: "#000000",
-	fontInverse: "#FFFFFF",
+	APP_BACKGROUND: "#fae8c0",
+	PRIMARY: "#660014",
+	SECONDARY: "#665959",
+	GREY: "#BDBDBD",
+	FONT_MAIN: "#292827",
+	FONT_INVERSE: "#FFFFFF",
 } as const;
 
 const COLORS_DARK = {
-	appBackground: "#262626",
-	primary900: "#BA68C8",
-	primary300: "#e1bee7",
-	primary200: "#514951",
-	primary100: "#351401",
-	secondary800: "#c86876",
-	grey300: "#757575",
-	grey600: "#BDBDBD",
-	fontMain: "#FFFFFF",
-	fontInverse: "#000000",
+	APP_BACKGROUND: "#292827",
+	PRIMARY: "#c86822",
+	SECONDARY: "#e8caa2",
+	GREY: "#BDBDBD",
+	FONT_MAIN: "#000000",
+	FONT_INVERSE: "#FFFFFF",
 } as const;
 
 export { COLORS_DARK, COLORS_LIGHT };
