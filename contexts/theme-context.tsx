795a7c981a55f6cd4561f13c2e16a7050962ac08
yulialutseeenko <lutsenkoyulia@ukr.net>
2024-07-31 23:16:24 +0300

    + added theme

--- contexts/theme-context.tsx
+++ contexts/theme-context.tsx
@@ -1,11 +1,12 @@
-import React, { createContext, useState, useEffect, ReactNode } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { createContext, useState, useEffect, ReactNode } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { COLORS_LIGHT, COLORS_DARK } from '../constants';
+import { COLORS_LIGHT, COLORS_DARK } from "../constants";
+import { THEME_STORAGE_KEY } from "../constants/config";
 
 type ThemeType = typeof COLORS_LIGHT | typeof COLORS_DARK;
 
-interface ThemeContextProps {
+export interface ThemeContextProps {
   theme: ThemeType;
   toggleTheme: () => void;
 }
@@ -15,10 +16,35 @@ const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
 const ThemeProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [theme, setTheme] = useState<ThemeType>(COLORS_LIGHT);
 
-  // Add logic to retrieve theme from AsyncStorage here
+  const loadTheme = async () => {
+    try {
+      const storedTheme = await AsyncStorage.getItem(THEME_STORAGE_KEY);
+      if (storedTheme) {
+        setTheme(storedTheme === "dark" ? COLORS_DARK : COLORS_LIGHT);
+      } else {
+        AsyncStorage.setItem(THEME_STORAGE_KEY, "light");
+        setTheme(COLORS_LIGHT);
+      }
+    } catch (error) {
+      console.error("Failed to load theme from AsyncStorage", error);
+    }
+  };
+
+  useEffect(() => {
+    loadTheme();
+  }, []);
 
   const toggleTheme = async () => {
-   // Add logic to toggle theme here
+    try {
+      const newTheme = theme === COLORS_LIGHT ? COLORS_DARK : COLORS_LIGHT;
+      setTheme(newTheme);
+      await AsyncStorage.setItem(
+        THEME_STORAGE_KEY,
+        newTheme === COLORS_LIGHT ? "light" : "dark"
+      );
+    } catch (error) {
+      console.error("Failed to save theme to AsyncStorage", error);
+    }
   };
 
   return (
