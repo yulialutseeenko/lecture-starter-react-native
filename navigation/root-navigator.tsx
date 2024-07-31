141facd7ab3d5c325cf7204923b32995364531ff
yulialutseeenko <lutsenkoyulia@ukr.net>
2024-07-31 23:19:00 +0300

    + added navigation

--- navigation/root-navigator.tsx
+++ navigation/root-navigator.tsx
@@ -1,26 +1,48 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
+import {
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { BottomTabsNavigator } from "./bottom-tabs-navigator";
 import { RootStackParamList } from "../types";
+import { useTheme } from "../hooks";
+import { AddInspiration } from "../screens";
+import { ROUTE_NAME } from "../enums";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const RootNavigator = () => {
-    const insets = useSafeAreaInsets();
+  const insets = useSafeAreaInsets();
+  const { theme } = useTheme();
 
-	return (
-		<SafeAreaView style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
-			<StatusBar />
-			<NavigationContainer>
-				<>
-					{/* Replace with navigator */}
-				</>
-			</NavigationContainer>
-		</SafeAreaView>
-	);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: theme.APP_BACKGROUND,
+          },
+          headerTintColor: theme.PRIMARY,
+          headerTitleStyle: {
+            fontFamily: "LobsterTwo-Regular",
+            fontSize: 26,
+            color: theme.PRIMARY,
+          },
+        }}
+      >
+        <Stack.Screen
+          name={ROUTE_NAME.BOTTOM_TABS_NAVIGATOR}
+          component={BottomTabsNavigator}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name={ROUTE_NAME.ADD_INSPIRATION}
+          component={AddInspiration}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export { RootNavigator };
